a4be383f1b2f97879c2c85777dbb2dc5023d2b69
Andrew Patterson <andrew.patterson747@gmail.com>
2021-07-14 16:57:21 -0500

    Fix expect tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,45 +4,57 @@ require './lib/user'
 RSpec.describe User do
 
   it 'exists' do
-  user_1 = User.new("Sal")
+    user_1 = User.new("Sal")
+    user_2 = User.new("Ali")
+    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
 
-    expect(user).to be_a(User)
+    expect(user_1).to be_a(User)
   end
 
-  xit 'calls name' do
-  user_1 = User.new("Sal")
-
+  it 'calls name' do
+    user_1 = User.new("Sal")
+    user_2 = User.new("Ali")
+    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+    
     expect(user_1.name).to eq("Sal")
   end
 
-  xit 'calls jokes' do
-  user_1 = User.new("Sal")
+  it 'calls jokes' do
+    user_1 = User.new("Sal")
+    user_2 = User.new("Ali")
+    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
 
     expect(user_1.jokes).to eq([])
   end
 
-  xit 'learns a joke' do
-  user_1 = User.new("Sal")
-  joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
-  joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+  it 'learns a joke' do
+    user_1 = User.new("Sal")
+    user_2 = User.new("Ali")
+    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
 
-    expect(user_1.learn(joke_1)).to eq([joke_1])
+    expect(user_1.learn(joke_1)).to eq(user_1.jokes)
+
   end
 
-  xit 'learns another joke' do
-  user_1 = User.new("Sal")
-  joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
-  joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+  it 'learns another joke' do
+    user_1 = User.new("Sal")
+    user_2 = User.new("Ali")
+    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+    expect(user_1.learn(joke_2)).to eq(user_1.jokes)
 
-    expect(user_1.learn(joke_2)).to eq([joke_2])
   end
 
-  xit 'shows how many jokes it knows' do
-  user_1 = User.new("Sal")
-  joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
-  joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
-
-    expect(user.jokes).to eq(@jokes)
+  it 'shows how many jokes it knows' do
+    user_1 = User.new("Sal")
+    user_2 = User.new("Ali")
+    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+    expect(user_1.jokes).to eq([])
   end
 
 
